b1833ef95c22743e06d97b13dede66b361c5b3db
Learner Learner <mehemmedmehdi2003@gmail.com>
2024-11-26 13:45:01 +0400

    ExModel with ReLU

--- models/exmodel.py
+++ models/exmodel.py
@@ -8,9 +8,11 @@ class ExModel(nn.Module):
 
         self.resnet18 = model.resnet18(pretrained=True)
         self.classifier = nn.Linear(1000, 53)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         with torch.no_grad():
             x = self.resnet18(x)
+            x = self.relu(x)
         x = self.classifier(x)
         return x;
